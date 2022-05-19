42150df9a6f90527826698d692cc6e5e7238718f
BenjaminThio <benjaminthioziliangmr.commandmg@gmail.com>
2022-05-19 23:58:47 +0800

    upgrading database system

--- Assets/Scripts/Recipes.cs
+++ Assets/Scripts/Recipes.cs
@@ -2,19 +2,60 @@ using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
 
+public class Database
+{
+    private static readonly object threadLock = new object();
+    private static Database database;
+    public int[] intArr { get; set; }
+    
+    public Database()
+    {
+        intArr = new int[]
+        {
+            1,
+            1,
+            1
+        };
+    }
+
+    public static Database Data()
+    {
+        lock (threadLock)
+        {
+            if (database == null)
+            {
+                database = new Database();
+            }
+            return database;
+        }
+    }
+
+    public void updateDatabase(Database data)
+    {
+        database = data;
+    }
+}
+
 public class Recipes : MonoBehaviour
 {
     void Awake()
     {
+        Save();
+    }
+
+    private void Save()
+    {
         print(Application.persistentDataPath);
+        Database database = Database.Data();
         string gameDataPath = $"{Application.persistentDataPath}/Path Of Chemistry";
         JsonSerializerSettings settings = new JsonSerializerSettings();
         settings.Formatting = Formatting.Indented;
         settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        var json = JsonConvert.SerializeObject(database, settings);
         foreach (string i in new string[]
         {
-            "Data",
-            "Freeze Data"
+            "Data"
+            //"Freeze Data"
         })
         {
             string directory = $"{gameDataPath}/{i}";
@@ -30,9 +71,9 @@ public class Recipes : MonoBehaviour
                 3,
                 4,
                 5
-            }, 
+            },
             settings);
-            File.WriteAllText(filePath, data);
+            File.WriteAllText(filePath, json);
         }
     }
 }
