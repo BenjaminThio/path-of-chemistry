084a2d699f5f1c99fa6afdc551c07a0714dc0904
BenjaminThio <benjaminthioziliangmr.commandmg@gmail.com>
2022-05-25 00:46:18 +0800

    Element constructor progress 50%

--- Assets/Scripts/ElementConstructor.cs
+++ Assets/Scripts/ElementConstructor.cs
@@ -1,29 +1,55 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ElementConstructor : MonoBehaviour
 {
     private Database db;
-    private int proton = 0;
-    private int electron = 0;
-    private int neutron = 0;
-    
+    private static Dictionary<string, int> particles = new Dictionary<string, int>()
+    {
+        {"Proton", 0},
+        {"Electron", 0},
+        {"Neutron", 0}
+    };
+
     private void Start()
     {
         db = Database.db;
+        UpdateSpecificParticle(gameObject.transform.parent.name);
     }
 
-    public void Add()
+    public void UpdateParticles(float value)
     {
         string particleName = gameObject.transform.parent.name;
-        GameObject.Find($"Element Constructor/{particleName}/Slider").GetComponent<Slider>().value += 1;
+        particles[particleName] = Convert.ToInt32(Mathf.Floor(value));
+        UpdateSpecificParticle(particleName);
     }
 
-    public void Remove()
+    public void AddOrRemove()
     {
         string particleName = gameObject.transform.parent.name;
-        GameObject.Find($"Element Constructor/{particleName}/Slider").GetComponent<Slider>().value -= 1;
+        if (gameObject.name == "Add")
+        {
+            if (particles[particleName] + 1 <= 118)
+            {
+                particles[particleName] += 1;
+            }
+        }
+        else if (gameObject.name == "Remove")
+        {
+            if (particles[particleName] - 1 >= 0)
+            {
+                particles[particleName] -= 1;
+            }
+        }
+        GameObject.Find($"Element Constructor/{particleName}/Slider").GetComponent<Slider>().value = particles[particleName];
+        UpdateSpecificParticle(particleName);
+    }
+
+    private void UpdateSpecificParticle(string particleName)
+    {
+        GameObject.Find($"Element Constructor/{particleName}/Input").GetComponent<TMP_InputField>().text = Convert.ToString(particles[particleName]);
     }
 }
